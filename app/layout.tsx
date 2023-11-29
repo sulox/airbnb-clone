4e31a89ffb6aa83b70887fc8509ce72395caab80
sulox <sulique@gmail.com>
2023-11-29 23:27:31 +0100

    just sync

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+//import Modal from './components/modals/Modal';
+import RegisterModal from './components/modals/RegisterModal';
+import ToasterProvider from './providers/ToasterProvider';
 
 //this provides the browser tab name once mouse-overed
 export const metadata: Metadata = {
@@ -19,7 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
